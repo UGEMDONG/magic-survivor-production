f92a88764299d22303502c20998cb07075564940
duffpqwjstk997 <duffpqwjstk997@gmail.com>
2026-05-28 21:31:38 +0900

    feature/inputSystem을 이용하여 플레이어 이동 구현
    ㅇㅇ

--- Assets/_Main/Scripts/Gameplay/Player/PlayerMove.cs
+++ Assets/_Main/Scripts/Gameplay/Player/PlayerMove.cs
@@ -1,16 +1,34 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PlayerMove : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float moveSpeed = 5f;
+    public InputActionReference moveAction; 
+    private Rigidbody2D rb;
+    private Vector2 KeepVector; // 대시 방향을 저장할 변
+
+    // 스크립트가 켜질 때 입력을 활성화합니다.
+    private void OnEnable()
     {
-        
+        if (moveAction != null)
+            moveAction.action.Enable();
+
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    // 스크립트가 꺼질 때 입력을 비활성화합니다. (메모리 낭비 방지)
+    private void OnDisable()
     {
-        
+        if (moveAction != null)
+            moveAction.action.Disable();
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 inputDirection = moveAction.action.ReadValue<Vector2>();
+        // 2D 게임 기준 (X, Y 축 이동)
+        Vector2 moveVector = new Vector2(inputDirection.x, inputDirection.y); 
+        rb.MovePosition(rb.position + moveVector * moveSpeed * Time.deltaTime);
     }
 }
